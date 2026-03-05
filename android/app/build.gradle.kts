6401542c5b7b3e03635f95885a3eb0e50bfeed42
episvr <2904825059@qq.com>
2026-03-05 19:36:26 +0800

    chore(android): bump version to 2.0.0 and rename package

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.velotask"
+    namespace = "top.episvr.velotask"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.velotask"
+        applicationId = "top.episvr.velotask"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
